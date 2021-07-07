295c13011eb1f54e9c538cbb55679a2e3adf06d0
KaiNeon <57714122+KaiNeon@users.noreply.github.com>
2021-07-07 20:15:16 +0500

    HochuDomoy

--- data/main.py
+++ data/main.py
@@ -1,31 +1,23 @@
 # Import
 import logging
+import json
 
 # Settings
-CONF_DIR = "test.txt"
+CONF_DIR = "test.json"
 CODE_DIR = ""
 
+
 # Functions
-def parse(file_dir=CONF_DIR):
-    result = [line.strip() for line in open(file_dir)]
+def get_data(file_dir=CONF_DIR):
+    with open(CONF_DIR, 'r', encoding='utf8') as file:
+        result = json.load(file)
     return result
+
 
 def mark(data_raw):
-    users_keyword = 'users:'
-    groups_keyword = 'groups:'
-    data_users = []
-    data_groups = []
-
-    if users_keyword not in data_raw:
-        logging.warning(" Keyword '" + users_keyword + "' not declared in your config file!")
-    if groups_keyword not in data_raw:
-        logging.warning(" Keyword '" + groups_keyword + "' not declared in your config file!")
-    if not (users_keyword in data_raw and groups_keyword in data_raw):
-        logging.error(" Keywords: '" + users_keyword + "' and '" + groups_keyword + "' not declared, process terminated.")
-        exit(1)
-
-    result = [data_users, data_groups]
-    return result
+    pass
+# result = [data_users, data_groups]
+# return result
 
 # Main
-print(mark(parse()))
+# print(parse())
