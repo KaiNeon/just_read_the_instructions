9f719b431344307e923953ed3513c27ad0235d81
KaiNeon <57714122+KaiNeon@users.noreply.github.com>
2021-07-13 16:33:07 +0500

    release with args support

--- data/main.py
+++ data/main.py
@@ -2,15 +2,27 @@
 import logging
 import json
 import os.path
+import argparse
 
 # Settings
+import sys
+import time
+
 CUR_DIR = os.getcwd()
+CONF_DIR = CUR_DIR
+STORE_DIR = os.path.join(os.path.abspath(os.path.join(CUR_DIR, os.pardir)), 'store')
 CONF_TEMP = "conf_"
 STORE_TEMP = "stored_"
 STORED_TEMP = CONF_TEMP + STORE_TEMP
-STORE_DIR = os.path.join(os.path.abspath(os.path.join(CUR_DIR, os.pardir)), 'store')
-NEW_CONFIG_FILE = "test.json"
-NEW_CONFIG_PATH = os.path.join(CUR_DIR, NEW_CONFIG_FILE)
+
+
+# Parser
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--store', default=STORE_DIR)
+    parser.add_argument('-i', '--input')
+    parser.add_argument('-o', '--output', default=CONF_DIR)
+    return parser
 
 
 # Functions
@@ -24,6 +36,8 @@ def get_current_ver(template=CONF_TEMP + STORE_TEMP):
     ver = 0
     if file_list:
         ver = max(file_list)
+    else:
+        logging.warning(f"Cannot find current version in {STORE_DIR}")
     return ver
 
 
@@ -36,16 +50,26 @@ def get_config_log_path(blink=0, filename=None, filedir=None):
     return filepath
 
 
-def get_data(file_path):
-    with open(file_path, 'r', encoding='utf8') as file:
-        return json.load(file)
-
-
 def create_config_log(data, filepath=None):
     if not filepath:
-        filepath = get_config_log_path()
+        filepath = get_config_log_path(1)
     with open(filepath, 'w', encoding='utf8') as file:
         json.dump(data, file, indent=2)
+
+
+def get_data(filepath):
+    try:
+        with open(filepath, 'r', encoding='utf8') as file:
+            return json.load(file)
+    except:
+        logging.error(f"No such file or directory {filepath}")
+        time.sleep(0.5)
+        if input("Should I create empty file as current version? [y if yes]") == 'y':
+            data = {'users': [], 'groups': []}
+            create_config_log(data, filepath)
+            return data
+        else:
+            exit(-1)
 
 
 def compare_part_asset(cur_ver_data, new_ver_data, partname, mirror=False):
@@ -63,10 +87,15 @@ def compare_part_asset(cur_ver_data, new_ver_data, partname, mirror=False):
 def compare_versions(cur_ver_path=None, new_ver_path=None):
     # Data collecting
     if not cur_ver_path:
-        cur_ver_path = get_config_log_path(1)
+        cur_ver_path = get_config_log_path()
     cur_ver_data = get_data(cur_ver_path)
     if not new_ver_path:
-        new_ver_path = NEW_CONFIG_PATH
+        logging.warning(f"No data in (new version path) {new_ver_path}")
+        time.sleep(0.5)
+        if input("Should I use 'test.json' as new version datafile? [y if yes]") == 'y':
+            new_ver_path = os.path.join(CUR_DIR, 'test.json')
+        else:
+            exit(-1)
     new_ver_data = get_data(new_ver_path)
 
     # Comparing
@@ -85,10 +114,12 @@ def compare_versions(cur_ver_path=None, new_ver_path=None):
     return ver_data
 
 
-def create_add_task(data=None):
+def create_add_task(data=None, output_dir=None):
     # Data collecting
     if not data:
         data = compare_versions()
+    if not output_dir:
+        output_dir = CUR_DIR
 
     # Keys translation (dicts)
     parts_match = {'users': 'user', 'groups': 'group'}
@@ -104,10 +135,12 @@ def create_add_task(data=None):
         return True
 
     # Writing task to .yml file
-    file = open(CONF_TEMP + str(get_current_ver()) + '.yml', 'w')
+    output_filename = CONF_TEMP + str(get_current_ver()) + '.yml'
+    file = open(os.path.join(output_dir, output_filename), 'w')
     file.write("---\n# Version: " + str(get_current_ver()) + "\ntasks:\n")
     for part in data.keys():
         for unit in data[part]:
+            file.write('\n')
             if 'remove' in unit.keys():
                 action = action_dict['remove']
             else:
@@ -124,5 +157,22 @@ def create_add_task(data=None):
     return False
 
 
+# Checking settings.json
+settings = get_data(os.path.join(CUR_DIR, 'settings.json'))
+if settings['store_dir']:
+    STORE_DIR = settings['store_dir']
+if settings['config_dir']:
+    CONF_DIR = settings['config_dir']
+if settings['config_template']:
+    CONF_TEMP = settings['config_template']
+if settings['stored_template']:
+    STORED_TEMP = settings['stored_template']
+
 # Init
-create_add_task()
+if __name__ == '__main__':
+    parser = create_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    STORE_DIR = namespace.store
+    create_add_task(compare_versions(get_config_log_path(), namespace.input), namespace.output)
+
+# create_add_task()
